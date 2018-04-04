905146c57508109a5d1659f2a26bea5ff4d7aa08
Patrick Riegler <patrick.riegler@intdev.ch>
2018-04-04 15:07:18 +0000

    fix the date display

--- restart.sh
+++ restart.sh
@@ -10,7 +10,7 @@ docker rm -fv php-api-app
 
 ## run:
 docker run -d -p 80:80 --name php-api-app php-api
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost/api.php)" != "200" ]]; do sleep 5; done'
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost/api.php)" != "200" ]]; do echo $(date); sleep 5; done'
 curl localhost/api.php?VIN=WDD2210561A233135
 
 
