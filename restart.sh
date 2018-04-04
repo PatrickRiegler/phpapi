5f75897f515f6aeb9560506a2aff059bd24a760d
Patrick Riegler <patrick.riegler@intdev.ch>
2018-04-04 14:58:26 +0000

    wait until container is started, before running the test case

--- restart.sh
+++ restart.sh
@@ -10,6 +10,7 @@ docker rm -fv php-api-app
 
 ## run:
 docker run -d -p 80:80 --name php-api-app php-api
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost/api.php)" != "200" ]]; do sleep 5; done'
 curl localhost/api.php?VIN=WDD2210561A233135
 
 
